fb979f7b14aa4a379de095b337af1adbb2af1f0e
salimp2009@gmail.com <salimp2009@gmail.com>
2023-03-28 13:11:19 +0300

    testing some rust configs for lvim

--- lvim/.config/lvim/lua/user/lsp/languages/rust.lua
+++ lvim/.config/lvim/lua/user/lsp/languages/rust.lua
@@ -24,7 +24,7 @@ pcall(function()
 			inlay_hints = {
 				auto = true,
 				only_current_line = false,
-				show_parameter_hints = false,
+				show_parameter_hints = true,
 				parameter_hints_prefix = "<-",
 				other_hints_prefix = "=>",
 				max_len_align = false,
