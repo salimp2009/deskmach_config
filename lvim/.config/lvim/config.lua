531d57fe93029de05b7b810648d880d12adf43a3
salimp2009@gmail.com <salimp2009@gmail.com>
2023-03-31 16:39:21 +0300

    added some exec-once in hypr.conf but it did not have any effect so disabled

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -132,13 +132,13 @@ linters.setup({
 	{
 		command = "markdownlint",
 	},
-	-- {
-	--   -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-	--   command = "shellcheck",
-	--   ---@usage arguments to pass to the formatter
-	--   -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-	--   extra_args = { "--severity", "warning" },
-	-- },
+	{
+		-- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+		command = "shellcheck",
+		---@usage arguments to pass to the formatter
+		-- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+		extra_args = { "--severity", "warning" },
+	},
 	-- {
 	--   command = "codespell",
 	--   ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
