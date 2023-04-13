450790c57a5b0e4b04c5c3d320d22f0c7e408630
salimp2009@gmail.com <salimp2009@gmail.com>
2023-04-13 13:41:41 +0300

    tried codeium keybings but did not work disabled it for the time being

--- lvim/.config/lvim/lua/user/codeium.lua
+++ lvim/.config/lvim/lua/user/codeium.lua
@@ -3,23 +3,31 @@ if not ok then
 	return
 end
 
-codeium.config = function()
-	vim.keymap.set("i", "<C-a>", function()
-		return vim.fn["codeium#Accept"]()
-	end, { expr = true })
+-- codeium.config = function()
+-- 	vim.keymap.set("i", "<C-a>", function()
+-- 		return vim.fn["codeium#Accept"]()
+-- 	end, { expr = true })
 
-	vim.keymap.set("i", "<c-j>", function()
-		return vim.fn["codeium#CycleCompletions"](1)
-	end, { expr = true })
+-- 	vim.keymap.set("i", "<c-j>", function()
+-- 		return vim.fn["codeium#CycleCompletions"](1)
+-- 	end, { expr = true })
 
-	vim.keymap.set("i", "<c-k>", function()
-		return vim.fn["codeium#CycleCompletions"](-1)
-	end, { expr = true })
+-- 	vim.keymap.set("i", "<c-k>", function()
+-- 		return vim.fn["codeium#CycleCompletions"](-1)
+-- 	end, { expr = true })
 
-	vim.keymap.set("i", "<c-x>", function()
-		return vim.fn["codeium#Clear"]()
-	end, { expr = true })
-end
+-- 	vim.keymap.set("i", "<c-x>", function()
+-- 		return vim.fn["codeium#Clear"]()
+-- 	end, { expr = true })
+-- end
 
 local sources_table = lvim.builtin.cmp.sources
 sources_table[#sources_table + 1] = { name = "codeium" }
+
+local opts = { noremap = true, silent = true }
+local keymap = vim.keymap.set
+
+keymap("i", "<c-j>", "<Cmd>call codeium#CycleCompletions(1)<CR>", opts)
+keymap("i", "<c-k>", "<Cmd>call codeium#CycleCompletions(-1)<CR>", opts)
+keymap("i", "<c-x>", "<Cmd>call codeium#Clear()<CR>", opts)
+keymap("i", "<c-a>", "<Cmd>call codeium#Accept()<CR>", opts)
