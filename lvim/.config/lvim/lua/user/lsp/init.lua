95c5a4b8c4344a424dd33c568505e824a97545fb
salimp2009@gmail.com <salimp2009@gmail.com>
2023-05-08 23:57:20 +0300

    test cpptools with gdb in lvim config; gdb debugging works better

--- lvim/.config/lvim/lua/user/lsp/init.lua
+++ lvim/.config/lvim/lua/user/lsp/init.lua
@@ -11,8 +11,7 @@ require("user.lsp.languages.cpp")
 
 -- require("user.lsp.languages.cpplldb")
 -- require("user.lsp.languages.cppgdb")
-
-lvim.lsp.diagnostics.virtual_text = false
+vim.diagnostic.config({ virtual_text = false })
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pylyzer" })
 -- if you don't want all the parsers change this to a table of the ones you want
 -- lvim.builtin.treesitter.ensure_installed = {
