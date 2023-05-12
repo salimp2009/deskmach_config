a07539e7bcf7d0c17986b4410c86d593127e39b6
salimp2009@gmail.com <salimp2009@gmail.com>
2023-05-12 21:57:51 +0300

    added more lvim config related to different languages

--- lvim/.config/lvim/after/ftplugin/python.lua
+++ lvim/.config/lvim/after/ftplugin/python.lua
@@ -3,6 +3,30 @@ if not status_ok then
 	return
 end
 
+-- Advanced pyright configuration
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright", "jsonls" })
+
+local pyright_opts = {
+	single_file_support = true,
+	settings = {
+		pyright = {
+			disableLanguageServices = false,
+			disableOrganizeImports = false,
+		},
+		python = {
+			analysis = {
+				autoImportCompletions = true,
+				autoSearchPaths = true,
+				diagnosticMode = "workspace", -- openFilesOnly, workspace
+				typeCheckingMode = "basic", -- off, basic, strict
+				useLibraryCodeForTypes = true,
+			},
+		},
+	},
+}
+
+require("lvim.lsp.manager").setup("pyright", pyright_opts)
+
 local opts = {
 	mode = "n", -- NORMAL mode
 	prefix = "<leader>",
