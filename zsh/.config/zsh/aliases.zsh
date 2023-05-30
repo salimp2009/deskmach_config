e03c195d189db286b22acdd5444cf19a88f075b5
salimp2009@gmail.com <salimp2009@gmail.com>
2023-05-31 00:09:55 +0300

    added wezterm and lvim related env variable and create lvim alias for it when using wezterm

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -7,6 +7,7 @@ alias zsh-update-plugins="find "$ZDOTDIR/plugins" -type d -exec test -e '{}/.git
 # alias yay="paru"
 
 # alias lvim='nvim -u ~/.local/share/lunarvim/lvim/init.lua --cmd "set runtimepath+=~/.local/share/lunarvim/lvim"'
+# alias lvim="env TERM=wezterm lvim"
 
 # get fastest mirrors
 alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
