eb428c085c805de04fea5c234596d47fc44bd486
salimp2009@gmail.com <salimp2009@gmail.com>
2023-06-17 18:33:30 +0300

    added autocommnd for nvim-tree to open with nvim .

--- nvim/.config/nvim/lua/user/autocommands.lua
+++ nvim/.config/nvim/lua/user/autocommands.lua
@@ -8,6 +8,25 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 	end,
 })
 
+vim.api.nvim_create_autocmd({ "VimEnter" }, 
+  { 
+   callback = function(data)
+    -- buffer is a directory
+      local directory = vim.fn.isdirectory(data.file) == 1
+
+      if not directory then
+        return
+      end
+
+      -- change to the directory
+      vim.cmd.cd(data.file)
+
+      -- open the tree
+      -- require("nvim-tree.api").tree.focus()
+      require("nvim-tree.api").tree.open()
+    end
+  })
+ 
 vim.api.nvim_create_autocmd({ "FileType" }, {
 	pattern = { "gitcommit", "markdown" },
 	callback = function()
---
+++ nvim/.config/nvim/lua/user/functions.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+ function M.open_nvim_tree(data)
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+return M
