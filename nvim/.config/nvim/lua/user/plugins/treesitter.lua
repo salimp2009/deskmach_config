f8cbdce351b7760d8b1b98cffb79965928bdbd4f
salimp2009@gmail.com <salimp2009@gmail.com>
2023-08-11 22:00:17 +0300

    added rainbow to treesitter in nvim

--- nvim/.config/nvim/lua/user/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/user/plugins/treesitter.lua
@@ -6,7 +6,13 @@ local M = {
 		{
 			"JoosepAlviste/nvim-ts-context-commentstring",
 		},
+		{
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
 		{ "windwp/nvim-ts-autotag" },
+		{
+			"p00f/nvim-ts-rainbow",
+		},
 		{
 			"kyazdani42/nvim-web-devicons",
 			config = function()
@@ -76,6 +82,43 @@ function M.config()
 		context_commentstring = {
 			enable = true,
 			enable_autocmd = false,
+		},
+		textobjects = {
+			select = {
+				enable = true,
+				-- Automatically jump forward to textobj, similar to targets.vim
+				lookahead = true,
+				keymaps = {
+					-- You can use the capture groups defined in textobjects.scm
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					["at"] = "@class.outer",
+					["it"] = "@class.inner",
+					["ac"] = "@call.outer",
+					["ic"] = "@call.inner",
+					["aa"] = "@parameter.outer",
+					["ia"] = "@parameter.inner",
+					["al"] = "@loop.outer",
+					["il"] = "@loop.inner",
+					["ai"] = "@conditional.outer",
+					["ii"] = "@conditional.inner",
+					["a/"] = "@comment.outer",
+					["i/"] = "@comment.inner",
+					["ab"] = "@block.outer",
+					["ib"] = "@block.inner",
+					["as"] = "@statement.outer",
+					["is"] = "@scopename.inner",
+					["aA"] = "@attribute.outer",
+					["iA"] = "@attribute.inner",
+					["aF"] = "@frame.outer",
+					["iF"] = "@frame.inner",
+				},
+			},
+		},
+		rainbow = {
+			enable = true,
+			extended_mode = false,
+			disable = { "html" },
 		},
 	})
 end
