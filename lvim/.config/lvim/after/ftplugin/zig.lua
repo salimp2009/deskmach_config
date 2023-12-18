445dda99a26da5bb66a4ce04498f606dffefc999
salimp2009@gmail.com <salimp2009@gmail.com>
2023-12-18 21:25:35 +0300

    adding minor tweaks

--- lvim/.config/lvim/after/ftplugin/zig.lua
+++ lvim/.config/lvim/after/ftplugin/zig.lua
@@ -3,19 +3,19 @@ require("lvim.lsp.manager").setup("zls", {
 	on_attach = require("lvim.lsp").common_on_attach,
 	capabilities = require("lvim.lsp").common_capabilities(),
 
-	-- schema = "https://raw.githubusercontent.com/zigtools/zls/master/schema.json",
-	-- -- edit to match zig compiler path
+	schema = "https://raw.githubusercontent.com/zigtools/zls/master/schema.json",
+	-- edit to match zig compiler path
 	-- zig_exe_path = "",--[[ "/home/xzhan/Development/Packages/software/zig/zig" ]]
-	-- enable_snippets = true,
-	-- enable_ast_check_diagnostics = true,
-	-- enable_autofix = false,
-	-- enable_import_embedfile_argument_completions = true,
-	-- warn_style = true,
-	-- enable_semantic_tokens = true,
-	-- enable_inlay_hints = true,
-	-- operator_completions = true,
-	-- include_at_in_builtins = false,
-	-- max_detail_length = 1048576,
+	enable_snippets = true,
+	enable_ast_check_diagnostics = true,
+	enable_autofix = false,
+	enable_import_embedfile_argument_completions = true,
+	warn_style = true,
+	enable_semantic_tokens = true,
+	enable_inlay_hints = true,
+	operator_completions = true,
+	include_at_in_builtins = false,
+	max_detail_length = 1048576,
 })
 
 -- zls.json setting either make zls.json at the root
