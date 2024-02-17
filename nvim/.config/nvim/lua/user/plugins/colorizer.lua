70183c14f569ebf972ed86aa3ed93282307e1703
salimp2009@gmail.com <salimp2009@gmail.com>
2024-02-17 21:44:30 +0300

    added eyeliner plugin to nvim

--- nvim/.config/nvim/lua/user/plugins/colorizer.lua
+++ nvim/.config/nvim/lua/user/plugins/colorizer.lua
@@ -13,7 +13,7 @@ function M.config()
 			RRGGBBAA = false, -- #RRGGBBAA hex codes
 			AARRGGBB = false, -- 0xAARRGGBB hex codes
 			rgb_fn = true, -- CSS rgb() and rgba() functions
-			hsl_fn = false, -- CSS hsl() and hsla() functions
+			hsl_fn = true, -- CSS hsl() and hsla() functions
 			css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB,
 			-- RRGGBB
 			css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
